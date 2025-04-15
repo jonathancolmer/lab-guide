f8d669d729cbdff4ed03c349720ab504d259dbef
kbbachman <kate.bachman.28@gmail.com>
2025-04-15 12:25:52 -0400

    added print functions, place to load data

--- example-code/r-example-code/modifying_columns.R
+++ example-code/r-example-code/modifying_columns.R
@@ -6,6 +6,11 @@
 install.packages("tidyverse")
 library(tidyverse)
 
+# Load data
+dataset <- read.csv("path/to/your/data")
+
+# Replace all column names in the following code to match your data. 
+
 # Create a variable named "new_variable" that equals 1 in every row
 
 #Conventional code
@@ -14,21 +19,29 @@ dataset$new_variable <- 1
 #Dplyr code
 dataset <- dataset %>%
   dplyr::mutate(new_variable=1)
+print(dataset)
 
 
-# Convert from character-to-numeric and numeric-to-character
-# Numeric variables are used for calculations, modeling, and quantitative 
-# analysis. Character variables are used for labels, categories, and 
-# qualitative information.
+# Convert from character-to-numeric and numeric-to-character and store as new
+# variables. Numeric variables are used for calculations, modeling, and 
+# quantitative analysis. Character variables are used for labels, categories,
+# and qualitative information.
+
+# If you want to overwrite the original variables entirely, change
+# "variable1_numeric" and "variable2_character" to the original variable names -
+# in this case, "variable1" and "variable2". 
+
 dataset <- dataset %>%
   dplyr::mutate(variable1_numeric = as.numeric(variable1),
                 variable2_character = as.character(variable2))
+print(dataset)
 
 
-# Extract a certain sequence of characters from a character string. This 
-# extracts the characters from position 4 to position 8 of the complete 
-# character string. 
+# Extract a certain sequence of characters from a character string and saves
+# as a new column in the dataset. This code extracts the characters from 
+# position 4 to position 8 of the complete character string. 
 dataset$character_snippet <- substr(dataset$character_complete, 4, 8)
+print(dataset)
 
 
 # If/else statements
@@ -42,6 +55,7 @@ dataset <- dataset %>%
                         NA)                    # Otherwise, assign NA.
   )
 
+print(dataset)
 
 # Create a variable whose name and value depend on the loop argument 
 # This can be helpful for reading or saving files in a folder. 
@@ -53,3 +67,6 @@ for(c in character_list){
   # Quotations do not show up in the cell values 
   dataset[[paste0("variable_", c)]] <- c  # Appends "variable_" and value in c
 }
+
+print(dataset)
+
